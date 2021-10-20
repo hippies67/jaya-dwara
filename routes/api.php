b9f97efd01198602b0d8320296e5a8c579dae32d
FIRST_NAME LAST_NAME <ikbal.malik26@gmail.com>
2021-10-20 10:43:36 +0700

    add api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\AchievementController;
+use App\Http\Controllers\Api\BannerController;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\ProfileWebController;
+use App\Http\Controllers\Api\TeamController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +27,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('products', [ProductController::class, 'index']);
 Route::get('projects', [ProjectController::class, 'index']);
+Route::get('achievements', [AchievementController::class, 'index']);
+Route::get('banners', [BannerController::class, 'index']);
+Route::get('events', [EventController::class, 'index']);
+Route::get('profile-web', [ProfileWebController::class, 'index']);
+Route::get('teams', [TeamController::class, 'index']);
